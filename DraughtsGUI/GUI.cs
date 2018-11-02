286cf1ad2ebcd019177c3cf2823ab7afcbb4d181
wrb06 <44703594+wrb06@users.noreply.github.com>
2018-11-02 16:50:19 +0000

    added basic board setup

---
+++ DraughtsGUI/GUI.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Draughts;
+
+namespace DraughtsGUI
+{
+    public partial class GUI : Form
+    {
+        const int scale = 50;
+        public GUI()
+        {
+            InitializeComponent();
+            Board board = new Board();
+            int i = 0;
+            foreach (Piece p in board.GetBoard())
+            {
+                TextBox txt = new TextBox();
+
+                txt.BackColor = (i % 2 + i / 8) % 2 == 0 ? Color.LightGray : Color.Gray;
+
+                
+                txt.Location = new Point((i%8)*scale, (i/8) * scale);
+                txt.AutoSize = false;
+                txt.Size = new Size(scale, scale);
+                txt.Enabled = false;
+                this.Controls.Add(txt);
+                i++;
+            }
+        }
+
+    }
+}
